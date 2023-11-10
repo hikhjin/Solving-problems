b4485b4b399841ac80c1eadee6fdfafd83763024
HyunJin Kim <55572372+hikhjin@users.noreply.github.com>
2023-11-10 17:50:02 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,16 +1,11 @@
 import sys
 input = sys.stdin.readline
-
 arr = [list(str(input().rstrip())) for _ in range(5)]
-length = max(len(s) for s in arr) # 문자열 길이 최댓값
-
 ans = ''
-for i in range(len(arr)):
-    if len(arr[i]) < length: # 최대 길이보다 짧을 경우 '' 추가
-        for j in range(length-len(arr[i])):
-            arr[i].append('')
 
-for i in range(length):
-    for j in range(len(arr)):
-        ans += arr[j][i]
+for i in range(15):
+    for j in range(5):
+        if i < len(arr[j]):
+            ans += arr[j][i]
+
 print(ans)
