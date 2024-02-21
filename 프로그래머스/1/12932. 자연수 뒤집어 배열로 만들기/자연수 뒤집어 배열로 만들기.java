e37801d7e46a10118fccb6ee8d44a662d059f023
HyunJin Kim <55572372+hikhjin@users.noreply.github.com>
2024-02-22 00:47:58 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 5.27 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(long n) {
+        int len = (n+"").length();
+        int[] answer = new int[len];
+        for (int i = 0; i < len; i++) {
+            answer[i] = (int) (n % 10);
+            n /= 10;
+        }
+        return answer;
+    }
+}
