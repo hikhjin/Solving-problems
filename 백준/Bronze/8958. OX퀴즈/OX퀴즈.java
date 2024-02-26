81b21102dee9c896a275ab871da6ca55b84599e3
HyunJin Kim <55572372+hikhjin@users.noreply.github.com>
2024-02-27 03:37:04 +0900

    [Bronze II] Title: OX퀴즈, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -9,29 +9,14 @@ public class Main {
         for (int i = 0; i < T; i++) {
             char[] arr = br.readLine().toCharArray();
             int ans = 0;
-            int tmp = 0;
+            int cnt = 0;
             for (int j = 0; j < arr.length; j++) {
                 if (arr[j] == 'O') {
-                    if (j != 0 && arr[j-1] == 'O') { // 연속 O 일 경우
-                        tmp++;
-                        if (j == arr.length-1) {
-                            if (tmp != 0) {
-                                for (int k = 2; k <= tmp + 1; k++) {
-                                    ans += k;
-                                }
-                            }
-                        }
-                    } else {
-                        ans++;
-                    }
-                } else { // X 일 경우
-                    if (tmp != 0) {
-                        for (int k = 2; k <= tmp + 1; k++) {
-                            ans += k;
-                        }
-                    }
-                    tmp = 0;
+                    cnt++;
+                } else {
+                    cnt = 0;
                 }
+                ans += cnt;
             }
             System.out.println(ans);
         }
