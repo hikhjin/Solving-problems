b25bb6f5836005a5bf7bd5afd513c421b300e3a6
HyunJin Kim <55572372+hikhjin@users.noreply.github.com>
2024-02-27 02:58:44 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 144 ms, Memory: 15932 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        char[] arr = br.readLine().toCharArray();
+        int[] ans = new int[26];
+
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = -1; // -1로 초기화
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (ans[arr[i]-97] == -1) { // 처음 나오는 알파벳만
+                ans[arr[i]-97] = i;
+            }
+        }
+        for (int an : ans) {
+            System.out.print(an + " ");
+        }
+    }
+}
