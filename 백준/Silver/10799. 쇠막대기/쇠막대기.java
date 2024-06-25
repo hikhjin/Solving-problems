32510f811e6bbdd4947db602a380384d6a5f16d5
김현진 <55572372+hikhjin@users.noreply.github.com>
2024-06-25 14:49:56 +0900

    [Silver II] Title: 쇠막대기, Time: 172 ms, Memory: 16380 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int ans = 0;
+        int cnt = 0; // 현재 막대기 개수
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+                cnt++;
+//                System.out.println("c='('");
+            } else {
+                if (s.charAt(i-1) == '(') { // 레이저
+                    cnt--;
+                    ans += cnt;
+//                    System.out.println("레이저");
+                } else {
+                    cnt--;
+                    ans++;
+//                    System.out.println("c=')'");
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
