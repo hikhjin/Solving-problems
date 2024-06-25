41203bd55c07954b637836531b2bf9327287bda1
김현진 <55572372+hikhjin@users.noreply.github.com>
2024-06-25 14:11:24 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,33 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < N; i++) {
-            String s = br.readLine(); // 한 줄 입력받기
-            Stack<Character> stack = new Stack<Character>(); // 한 글자씩 확인하므로 Character
-
-            for(int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == '(') { // 문자열.charAt(숫자) -> 문자열의 숫자번째 문자를 반환
-                    stack.push(s.charAt(j)); // '(' 면 스택에 push
-                } else if (stack.empty()) { // ')' 일 때 스택이 비어있으면 스택에 push하고 반복문 빠져나가기
-                    stack.push(s.charAt(j));
-                    break;
+        for (int i = 0; i < N; i++) {
+            Stack<Character> stack = new Stack<Character>();
+            boolean flag = true;
+            String s = br.readLine();
+            for (char c : s.toCharArray()) {
+                if (c == '(') {
+                    stack.push(c);
                 } else {
-                    stack.pop(); // ')'일 때 스택이 비어 있지 않으면 pop
+                    if (stack.isEmpty()) {
+                        flag = false;
+                        break;
+                    } else {
+                        stack.pop();
+                    }
                 }
             }
-            if (stack.empty()) {
-                System.out.println("YES");
+
+            if (!stack.isEmpty()) {
+                flag = false;
+            }
+
+            if (flag) {
+                sb.append("YES\n");
             } else {
-                System.out.println("NO");
+                sb.append("NO\n");
             }
         }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
     }
 }
