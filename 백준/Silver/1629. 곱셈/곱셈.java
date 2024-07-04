3cd118ee5c281774c4de06f02173694f159d3835
김현진 <55572372+hikhjin@users.noreply.github.com>
2024-07-04 18:28:59 +0900

    [Silver I] Title: 곱셈, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -4,25 +4,28 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static long A, B, C;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        A = Long.parseLong(st.nextToken());
-        B = Long.parseLong(st.nextToken());
-        C = Long.parseLong(st.nextToken());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
 
         System.out.println(solve(A, B, C));
     }
+
     static long solve(long A, long B, long C) {
-        if (B == 0) {
-            return 1;
+        if (B == 0) { // B가 0이면 거듭제곱값은 항상 1
+            if (C == 1) {
+                return 0;
+            } else {
+                return 1;
+            }
         }
         long half = solve(A, B/2, C); // 분할 정복
         long res = (half * half) % C;
 
-        if (B % 2 == 1) {
+        if (B % 2 == 1) { // 홀수면 한번 더
             res = (res * A) % C;
         }
         return res;
