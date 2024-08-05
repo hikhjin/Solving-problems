823dd1b61a306b7f0774e959e0181f886eea9a60
김현진 <55572372+hikhjin@users.noreply.github.com>
2024-08-05 19:45:58 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 2736 ms, Memory: 45620 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
@@ -8,6 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
 
@@ -43,8 +42,9 @@ public class Main {
                     }
                 }
             }
-
-            System.out.println(cnt);
+            bw.write(cnt + "\n");
         }
+        bw.flush();
+        bw.close();
     }
 }
