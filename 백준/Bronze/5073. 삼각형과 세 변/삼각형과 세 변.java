1131ef3c875e297549c123709580fcec480b72a1
김현진 <55572372+hikhjin@users.noreply.github.com>
2024-09-09 21:17:03 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 92 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -5,13 +5,14 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         int[] arr= new int[3];
         while(true) {
             st = new StringTokenizer(br.readLine());
-
-
+            
             arr[0] = Integer.parseInt(st.nextToken());
             arr[1] = Integer.parseInt(st.nextToken());
             arr[2] = Integer.parseInt(st.nextToken());
@@ -21,17 +22,18 @@ public class Main {
             if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0) break;
 
             else if(arr[2] >= arr[0] + arr[1]) {
-                System.out.println("Invalid");
+                sb.append("Invalid").append("\n");
             }else if(arr[0] == arr[1] && arr[1] == arr[2]) {
-                System.out.println("Equilateral");
+                sb.append("Equilateral").append("\n");
             }else if(arr[0] == arr[1] || arr[1] == arr[2] || arr[0] == arr[2]) {
-                System.out.println("Isosceles");
+                sb.append("Isosceles").append("\n");
             }else {
-                System.out.println("Scalene");
+                sb.append("Scalene").append("\n");
             }
-
-
         }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 
 }
