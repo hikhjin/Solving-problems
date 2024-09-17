806f87538b8aee25cdaf236a88ff625adc8876cf
김현진 <55572372+hikhjin@users.noreply.github.com>
2024-09-18 02:41:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 293.31 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -6,8 +6,6 @@ class Solution {
         Arrays.sort(completion);
         
         for (int i = 0; i < completion.length; i++) {
-            // System.out.println(participant[i]);
-            // System.out.println(completion[i]);
             if (!participant[i].equals(completion[i])) {
                 return participant[i];
             }
