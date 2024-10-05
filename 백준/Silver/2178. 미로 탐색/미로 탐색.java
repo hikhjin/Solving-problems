3a549325f4c53570a4f0901ad75beaec3c53c10e
김현진 <55572372+hikhjin@users.noreply.github.com>
2024-10-05 22:40:50 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14612 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -6,52 +6,47 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[][] maze;
+    static boolean[][] visited;
     static int N, M;
-    static int[][] arr;
-    static int[][] dist;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        maze = new int[N][M];
+        visited = new boolean[N][M];
 
-        arr = new int[N][M];
-        dist = new int[N][M];
-
-        for (int i = 0; i < N; i++) { // 배열에 값 입력
+        for (int i = 0; i < N; i++) {
             String s = br.readLine();
             for (int j = 0; j < M; j++) {
-                arr[i][j] = s.charAt(j) - '0'; // char -> int
-                dist[i][j] = -1; // -1로 초기화 (방문 x)
+                maze[i][j] = s.charAt(j) - '0';
             }
         }
-
-        BFS(0,0);
-        System.out.println(dist[N-1][M-1]);
+        bfs(0,0);
+        System.out.println(maze[N-1][M-1]);
     }
 
-    static void BFS(int x, int y) {
+    static void bfs(int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
+        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-        queue.offer(new int[] {x, y});
-        dist[x][y] = 1; // 시작점의 거리를 1로 설정
+        queue.offer(new int[]{x, y});
+        visited[x][y] = true;
 
         while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
+            int[] cur = queue.poll();
 
-            for (int i = 0; i < 4; i++) {
-                int nx = poll[0] + dx[i];
-                int ny = poll[1] + dy[i];
+            for (int[] dir : dirs) {
+                int nx = cur[0] + dir[0];
+                int ny = cur[1] + dir[1];
 
                 if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                    if (arr[nx][ny] == 1 && dist[nx][ny] == -1) {
-                        queue.offer(new int[] {nx, ny});
-                        dist[nx][ny] = dist[poll[0]][poll[1]] + 1;
+                    if (maze[nx][ny] == 1 && !visited[nx][ny]) {
+                        queue.offer(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                        maze[nx][ny] = maze[cur[0]][cur[1]] + 1;
                     }
-
                 }
             }
         }
