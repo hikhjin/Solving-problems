1d4249c72f187837d3dc03fc560deb2f65f91376
김현진 <55572372+hikhjin@users.noreply.github.com>
2024-10-06 19:48:18 +0900

    [Silver III] Title: N과 M (1), Time: 224 ms, Memory: 23584 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,39 +1,40 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr = new int[10]; //
-    static boolean[] check = new boolean[10]; // 사용한 수 확인하는 배열
-    static StringBuilder sb  = new StringBuilder();
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
 
-        solve(N, M, 0);
+        int N = Integer.parseInt(st.nextToken()); // 1부터 n까지
+        int M = Integer.parseInt(st.nextToken()); // 중복 없이 M개 고른 수열
+        int[] arr = new int[M + 1]; // 수열 저장할 배열
+        boolean[] check = new boolean[N + 1]; // 사용한 수 확인하는 배열
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        backtrack(N, M, 0, arr, check);
+
+        System.out.println(sb);
 
     }
 
-    static void solve(int N, int M, int K) {
-        if (K == M) {
-           for (int i = 0; i < M; i++) {
-               sb.append(arr[i] + " ");
-           }
-           sb.append("\n");
-           return;
+    static void backtrack(int N, int M, int idx, int[] arr, boolean[] check) {
+        if (idx == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
         }
 
         for (int i = 1; i <= N; i++) {
             if (!check[i]) {
-                arr[K] = i;
                 check[i] = true;
-                solve(N, M, K + 1);
+                arr[idx] = i;
+                backtrack(N, M, idx + 1, arr, check);
                 check[i] = false;
             }
         }
