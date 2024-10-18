79d5e8d19874c9f90cab5231d94736da48834fb8
김현진 <55572372+hikhjin@users.noreply.github.com>
2024-10-19 00:10:53 +0900

    [Gold IV] Title: 빙산, Time: 560 ms, Memory: 195284 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,21 +1,20 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
     static int[][] arr;
-    static boolean[][] visited;
-    static int[][] melting; // 빙하 녹을 때 계산 용도
+    static int N, M;
+    static int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         arr = new int[N][M];
-        melting = new int[N][M];
-        int time = 0; // 걸린 시간
-
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
@@ -23,65 +22,74 @@ public class Main {
             }
         }
 
+        int days = 0;
         while (true) {
-            visited = new boolean[N][M];
-            int cnt = 0; // 빙산의 개수
-            for (int i = 0; i < N; i++) { // 빙산 개수 세기
+            days++;
+            int cnt = 0;
+            melting();
+
+            boolean[][] visited = new boolean[N][M];
+            for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
-                    if (!visited[i][j] && arr[i][j] != 0) {
-                        BFS(i,j);
+                    if (arr[i][j] != 0 && !visited[i][j]) {
+                        bfs(i, j, visited);
                         cnt++;
                     }
                 }
             }
-
-            if (cnt > 1) {
-                System.out.println(time);
-                break;
-            } else if (cnt == 0) { // 다 녹았을 경우
+            if (cnt >= 2) {
+                System.out.println(days);
+                return;
+            }
+            if (cnt == 0) {
                 System.out.println(0);
-                break;
-            }
-            calMelt();
-            time++;
-        }
-    }
-
-    static void BFS(int x, int y) { // 빙산 개수 세는 함수
-        Queue<int[]> q = new LinkedList<>();
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        q.offer(new int[]{x, y});
-        visited[x][y] = true;
-
-        while (!q.isEmpty()) {
-            int[] poll = q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = poll[0] + dx[i];
-                int ny = poll[1] + dy[i];
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                    if (arr[nx][ny] != 0 && !visited[nx][ny]) {
-                        visited[nx][ny] = true;
-                        q.offer(new int[]{nx, ny});
-                    }
-                    if (arr[nx][ny] == 0) { // 주위가 바다일 경우
-                        melting[poll[0]][poll[1]]++; // 깎아지는 만큼 melting에 추가
-                    }
-                }
+                return;
             }
         }
     }
 
-    static void calMelt() {
+    static void melting() {
+        int[][] tmpArr = new int[N][M];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                arr[i][j] -= melting[i][j]; // 녹은 만큼 빼줌
-                if (arr[i][j] < 0) { // 음수면 0
-                    arr[i][j] = 0;
+                tmpArr[i][j] = arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (tmpArr[i][j] != 0) {
+                    int cnt = 0;
+                    for (int[] dir : dirs) {
+                        int x = i + dir[0];
+                        int y = j + dir[1];
+                        if (x >= 0 && x < N && y >= 0 && y < M && tmpArr[x][y] == 0) cnt++;
+                    }
+                    if (tmpArr[i][j] - cnt >= 0) arr[i][j] = tmpArr[i][j] - cnt;
+                    else arr[i][j] = 0;
                 }
-                melting[i][j] = 0; // 다음 턴에 다시 사용해야 하므로 초기화
+            }
+        }
+    }
+
+    static void bfs(int x, int y, boolean[][] visited) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            for (int[] dir : dirs) {
+                int nx = cur[0] + dir[0];
+                int ny = cur[1] + dir[1];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                    if (arr[nx][ny] != 0 && !visited[nx][ny]) {
+                        queue.offer(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                    }
+                }
             }
         }
     }
