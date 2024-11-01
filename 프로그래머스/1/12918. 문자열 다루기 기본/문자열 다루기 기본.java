64e8cb09d7e6270a6ec6e34808f150f514630ede
김현진 <55572372+hikhjin@users.noreply.github.com>
2024-11-01 20:33:13 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(String s) {
+        int len = s.length();
+        if (len != 4 && len != 6) {
+            return false;
+        }
+        
+        for (int i = 0; i < len; i++) {
+                if (s.charAt(i) - '0' < 0 || s.charAt(i) - '9' > 9) {
+                    return false;
+                }
+            }
+        return true;
+    }
+}
