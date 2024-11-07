b62c5e9f35d1a64710e0255bd5e208ad29b4ab04
김현진 <55572372+hikhjin@users.noreply.github.com>
2024-11-08 02:51:32 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 39.06 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,36 +2,41 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        int truck_weight = 0; // 다리 위에 올라가 있는 트럭 무게
+        Queue<Truck> queue = new LinkedList<>(); // 다리 위 트럭
+        int sec = 1;
+        int idx = 1;
+        int sum = truck_weights[0];
+        queue.offer(new Truck(truck_weights[0], 1));
         
-        Queue<Integer> queue_bridge = new LinkedList<>(); // 다리 위에 있는 트럭 큐
-        Queue<Integer> queue_wait = new LinkedList<>(); // 대기하는 트럭 큐
-        
-        for (int i = 0; i < bridge_length; i++) {
-            queue_bridge.offer(0); // 다리 위에 있는 트럭 큐 초기화
-        }
-        
-        for (int i = 0 ; i < truck_weights.length; i++) {
-            queue_wait.offer(truck_weights[i]); // 대기하는 트럭 큐에 추가
-        }
-        
-        while (!queue_bridge.isEmpty()) {
-            answer++;
-            truck_weight -= queue_bridge.poll(); // 다리 위 맨 앞 트럭 무게 빼줌
+        while (!queue.isEmpty()) {
+            if (queue.peek().pos == bridge_length) {
+                sum -= queue.poll().w;
+            }
             
-            if (!queue_wait.isEmpty()) {
-                if (truck_weight + queue_wait.peek() <= weight) {
-                    int tmp = queue_wait.poll();
-                    truck_weight += tmp; // 대기 중인 맨 앞 트럭 다리 위에 추가
-                    queue_bridge.offer(tmp);
-                    } else {
-                    queue_bridge.offer(0);
+            if (idx < truck_weights.length) {
+                if (sum + truck_weights[idx] <= weight) {
+                    sum += truck_weights[idx];
+                    queue.offer(new Truck(truck_weights[idx], 0));
+                    idx++;
                 }
             }
-                
+            
+            for (Truck t : queue) {
+                t.pos++;
+            }
+            sec++;
         }
         
-        return answer;
+        return sec;
+    }
+}
+
+class Truck {
+    int w;
+    int pos;
+    
+    public Truck(int w, int pos) {
+        this.w = w;
+        this.pos = pos;
     }
 }
