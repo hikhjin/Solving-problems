f622d79d85aafeb39591237e3750c2191a07c017
김현진 <55572372+hikhjin@users.noreply.github.com>
2024-11-15 18:22:46 +0900

    [D3] Title: 햄버거 다이어트, Time: 192 ms, Memory: 20,088 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -4,48 +4,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int maxScore;
-    static int limitCal;
     static int N;
-    static int[][] hamburger;
+    static int limitCal;
+    static int[][] arr;
+    static int maxScore = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-
-        for (int i = 1; i <= T; i++) {
-            sb.append("#").append(i).append(" ");
+        for (int t = 1; t <= T; t++) {
+            sb.append("#" + t + " ");
             st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            limitCal = Integer.parseInt(st.nextToken());
+            arr = new int[N][2];
 
-            maxScore = 0;
-            N = Integer.parseInt(st.nextToken()); // 음식 수
-            limitCal = Integer.parseInt(st.nextToken()); // 제한 칼로리
-            hamburger = new int[N][N];
-            for (int j = 0; j < N; j++) {
+            for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                hamburger[j][0] = Integer.parseInt(st.nextToken()); // 점수
-                hamburger[j][1] = Integer.parseInt(st.nextToken()); // 칼로리
+                arr[i][0] = Integer.parseInt(st.nextToken());
+                arr[i][1] = Integer.parseInt(st.nextToken());
             }
-
             dfs(0, 0, 0);
             sb.append(maxScore);
-            if (i != T) sb.append("\n");
+            if (t != T) sb.append("\n");
+            maxScore = 0;
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
-    static void dfs(int score, int calSum, int idx) {
-        if (calSum > limitCal) return;
+    static void dfs(int sumCal, int idx, int score) {
+        if (sumCal > limitCal) return;
         if (idx == N) {
-            if (calSum <= limitCal) {
-                maxScore = Math.max(maxScore, score);
-                return;
-            }
-        };
+            maxScore = Math.max(maxScore, score);
+            return;
+        }
 
-        dfs(score+hamburger[idx][0], calSum+hamburger[idx][1], idx+1);
-        dfs(score, calSum, idx+1);
+        dfs(sumCal, idx + 1, score);
+        dfs(sumCal + arr[idx][1], idx + 1, score + arr[idx][0]);
     }
 }
