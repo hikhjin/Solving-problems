540ab3a590b9259a7991b22b706dde1b68c487dc
김현진 <55572372+hikhjin@users.noreply.github.com>
2025-04-08 21:49:36 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.74 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,58 +3,52 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] wires) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
-        int answer = Integer.MAX_VALUE;
-
-        // 그래프 초기화
+        int ans = Integer.MAX_VALUE;
+        
         for (int i = 1; i <= n; i++) {
             graph.put(i, new ArrayList<>());
         }
-
-        // 그래프에 엣지 추가
-        for (int[] wire : wires) {
+        
+        for (int[] wire: wires) {
             graph.get(wire[0]).add(wire[1]);
             graph.get(wire[1]).add(wire[0]);
         }
-
-        // 각 엣지를 한 번씩 제거하고 최소 차이 찾기
+        
         for (int[] wire : wires) {
-            // 전선 끊기
             graph.get(wire[0]).remove(Integer.valueOf(wire[1]));
             graph.get(wire[1]).remove(Integer.valueOf(wire[0]));
-
-            // BFS 탐색
-            boolean[] visited = new boolean[n + 1];
-            int cnt1 = BFS(graph, wire[0], visited);
-
-            // 다시 전선 붙이기
+            
+            boolean[] visited = new boolean[n+1];
+            int cnt1 = bfs(graph, wire[0], visited);
+            int cnt2 = n - cnt1;
+            
             graph.get(wire[0]).add(wire[1]);
             graph.get(wire[1]).add(wire[0]);
-
-            // 두 서브 그래프의 차이 계산
-            int cnt2 = n - cnt1;
-            answer = Math.min(answer, Math.abs(cnt1 - cnt2));
+            
+            ans = Math.min(Math.abs(cnt1-cnt2), ans);
+            
         }
-        return answer;
+        return ans;
     }
-
-    // BFS 함수
-    int BFS(Map<Integer, List<Integer>> graph, int start, boolean[] visited) {
+    
+    static int bfs(Map<Integer, List<Integer>> graph, int start, boolean[] visited) {
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(start);
         visited[start] = true;
         int cnt = 1;
-
+        
         while (!queue.isEmpty()) {
-            int poll = queue.poll();
-
-            for (int next : graph.get(poll)) {
+            int cur = queue.poll();
+            
+            for (int next : graph.get(cur)) {
                 if (!visited[next]) {
-                    visited[next] = true;
                     queue.offer(next);
+                    visited[next] = true;
                     cnt++;
                 }
             }
         }
+        
         return cnt;
     }
 }
