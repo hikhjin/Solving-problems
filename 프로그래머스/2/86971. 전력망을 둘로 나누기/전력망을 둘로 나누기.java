8e8b5929d1b8d1c84d54749f39dba30e76a98a9a
김현진 <55572372+hikhjin@users.noreply.github.com>
2025-04-09 18:35:33 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.51 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,20 +1,22 @@
 import java.util.*;
 
 class Solution {
+    static Map<Integer, List<Integer>> graph = new HashMap<>();
+    
     public int solution(int n, int[][] wires) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        int ans = Integer.MAX_VALUE;
-        
         for (int i = 1; i <= n; i++) {
             graph.put(i, new ArrayList<>());
         }
         
-        for (int[] wire: wires) {
+        for (int[] wire : wires) {
             graph.get(wire[0]).add(wire[1]);
             graph.get(wire[1]).add(wire[0]);
         }
         
+        int min = Integer.MAX_VALUE;
+        
         for (int[] wire : wires) {
+            // 전선 끊기
             graph.get(wire[0]).remove(Integer.valueOf(wire[1]));
             graph.get(wire[1]).remove(Integer.valueOf(wire[0]));
             
@@ -22,33 +24,32 @@ class Solution {
             int cnt1 = bfs(graph, wire[0], visited);
             int cnt2 = n - cnt1;
             
+            min = Math.min(min, Math.abs(cnt1-cnt2));
+            
+            // 다시 복구
             graph.get(wire[0]).add(wire[1]);
             graph.get(wire[1]).add(wire[0]);
-            
-            ans = Math.min(Math.abs(cnt1-cnt2), ans);
-            
         }
-        return ans;
+        return min;
     }
     
     static int bfs(Map<Integer, List<Integer>> graph, int start, boolean[] visited) {
         Queue<Integer> queue = new LinkedList<>();
+        int cnt = 0;
         queue.offer(start);
         visited[start] = true;
-        int cnt = 1;
         
         while (!queue.isEmpty()) {
             int cur = queue.poll();
+            cnt++;
             
             for (int next : graph.get(cur)) {
                 if (!visited[next]) {
                     queue.offer(next);
                     visited[next] = true;
-                    cnt++;
                 }
             }
         }
-        
         return cnt;
     }
 }
