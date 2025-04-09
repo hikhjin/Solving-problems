ecead79b91564cc2c896aed09f7808bc39842bf3
김현진 <55572372+hikhjin@users.noreply.github.com>
2025-04-09 16:36:10 +0900

    [level 2] Title: 땅따먹기, Time: 10.38 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,10 +1,12 @@
 class Solution {
     int solution(int[][] land) {
-        for (int i = 1; i < land.length; i++) {
+        int N = land.length;
+        
+        for (int i = 1; i < N; i++) {
             for (int j = 0; j < 4; j++) {
                 int max = 0;
                 for (int k = 0; k < 4; k++) {
-                    if (j == k) continue;
+                    if (k == j) continue;
                     max = Math.max(max, land[i-1][k]);
                 }
                 land[i][j] += max;
@@ -13,7 +15,7 @@ class Solution {
         
         int max = 0;
         for (int i = 0; i < 4; i++) {
-            max = Math.max(land[land.length-1][i], max);
+            max = Math.max(land[N-1][i], max);
         }
         return max;
     }
