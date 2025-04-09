c6b6a93607c9a41695053932f3243e47f15e3153
김현진 <55572372+hikhjin@users.noreply.github.com>
2025-04-09 19:08:23 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 100.68 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,58 +1,56 @@
 import java.util.*;
 
 class Solution {
+    static int n, m;
+    static int[] oil;
     static boolean[][] visited;
-    static int row, col;
-    static int[] oil; // 열별 석유량 저장
-
+    
     public int solution(int[][] land) {
-        row = land.length;
-        col = land[0].length;
-        oil = new int[col];
-        visited = new boolean[row][col];
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        n = land.length; // 세로
+        //System.out.println(n);
+        m = land[0].length; // 가로
+        //System.out.println(m);
+        visited = new boolean[n][m];
+        oil = new int[m];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (!visited[i][j] && land[i][j] == 1) {
-                    bfs(i, j, land);
+                    bfs(land, i, j);
                 }
             }
         }
-
-        int ans = 0;
-        for (int i = 0; i < col; i++) {
-            ans = Math.max(ans, oil[i]);
-        }
-        return ans;
+        
+        Arrays.sort(oil);
+        return oil[m-1];
     }
-
-    static void bfs(int x, int y, int[][] land) {
+    
+    static void bfs(int[][] land, int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
-        int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        queue.offer(new int[]{x, y});
+        int[][] dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+        queue.offer(new int[]{x,y});
         visited[x][y] = true;
-
-        int cnt = 0;
         Set<Integer> set = new HashSet<>();
-
+        int cnt = 0;
+        
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
+            set.add(cur[1]);
             cnt++;
-            set.add(cur[1]); // 열 번호 저장
-
+            
             for (int[] dir : dirs) {
-                int nx = cur[0] + dir[0];
-                int ny = cur[1] + dir[1];
-
-                if (nx >= 0 && nx < row && ny >= 0 && ny < col) {
+                int nx = dir[0] + cur[0];
+                int ny = dir[1] + cur[1];
+                
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
                     if (!visited[nx][ny] && land[nx][ny] == 1) {
-                        queue.offer(new int[]{nx, ny});
+                        queue.offer(new int[] {nx, ny});
                         visited[nx][ny] = true;
                     }
                 }
             }
         }
-
+        
         for (int s : set) {
             oil[s] += cnt;
         }
