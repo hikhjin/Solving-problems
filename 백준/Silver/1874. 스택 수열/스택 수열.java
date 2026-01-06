4ba7050d24bef8e7bf017b7923a89458f0d6709b
김현진 <55572372+hikhjin@users.noreply.github.com>
2026-01-06 23:23:03 +0900

    [Silver II] Title: 스택 수열, Time: 300 ms, Memory: 26900 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -6,24 +6,25 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int K = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
         int idx = 1;
 
-        for (int i = 1; i <= K; i++) {
-            int n = Integer.parseInt(br.readLine());
-
-            while (idx <= n) {
+        for (int i = 1; i <= n; i++) {
+            int now = Integer.parseInt(br.readLine());
+            
+            while (idx <= now) {
                 stack.push(idx);
-                idx++;
                 sb.append("+\n");
+                idx++;
             }
 
-            if (stack.peek() == n) {
+            if (stack.peek() == now) {
                 stack.pop();
                 sb.append("-\n");
-            } else {
+            }  else {
                 System.out.println("NO");
                 return;
             }
