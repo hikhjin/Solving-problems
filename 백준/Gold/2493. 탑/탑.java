5bba404b7ffeb1b68d5feafd87ffd84748dfe17b
김현진 <55572372+hikhjin@users.noreply.github.com>
2026-01-11 00:07:24 +0900

    [Gold V] Title: 탑, Time: 860 ms, Memory: 93000 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[]{Integer.parseInt(st.nextToken()), 1}); // 첫번째꺼 넣기, 이때 무조건 0
+        sb.append(0).append(" ");
+
+        for (int i = 1; i < n; i++) {
+            int now = Integer.parseInt(st.nextToken());
+            if (now < stack.peek()[0]) {
+                sb.append(stack.peek()[1]).append(" ");
+            } else {
+                while (now >= stack.peek()[0]) {
+                    stack.pop();
+                    if (stack.isEmpty()) {
+                        sb.append(0).append(" ");
+                        break;
+                    }
+                }
+                if (!stack.isEmpty()) {
+                    sb.append(stack.peek()[1]).append(" ");
+                }
+            }
+            stack.push(new int[]{now, i+1});
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+}
